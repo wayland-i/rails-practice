b5675f89ed39df6843d9776a0b602c5aaeff437b
Ian Iversen <ianiversen1@gmail.com>
2023-01-29 12:00:19 -0500

    feat: pessimistic render sandbox

--- scaffold-test/client/scaffold-test-client/pages/index.tsx
+++ scaffold-test/client/scaffold-test-client/pages/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  const [posts, setPosts] = useState([])
+
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then(r => {
+        if (!r.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        return r.json();
+      })
+      .then(data => setPosts(data))
+      .catch(error => {
+        console.error(error.message);
+      });
+  }, []);
+
+  console.log(posts);
+
+
   return (
     <div>
       <h1>Homepage</h1>
